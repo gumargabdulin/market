d2e9e6156240d90e39d9861a1ed8643bffec78f9
Gumar <gumar.gabdulin@yandex.ru>
2025-07-01 14:41:57 +0500

    Adminka front zagatovka

---
+++ resources/js/Layouts/AdminLayout.vue
@@ -0,0 +1,32 @@
+<template>
+    <section class="w-full border-b bg-grey-50 p-4">
+        <header>
+            Market Admin
+        </header>
+    </section>
+    <section class="flex">
+        <aside class="w-1/4 bg-indigo-50 min-h-screen">
+            <nav>
+
+            </nav>
+        </aside>
+        <article class="w-3/4 bg-gray-100">
+            <slot/>
+        </article>
+    </section>
+    <section>
+        <footer>
+
+        </footer>
+    </section>
+</template>
+<script>
+export default {
+    name: "AdminLayout"
+}
+</script>
+
+
+<style scoped>
+
+</style>
---
+++ resources/js/Pages/Admin/Category/Index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        Category
+    </div>
+</template>
+<script>
+import AdminLayout from "@/Layouts/AdminLayout.vue";
+
+export default {
+    name: "Index",
+    layout: AdminLayout
+}
+</script>
+
+
+
+<style scoped>
+
+</style>
